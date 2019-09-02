cd37422ad3b38d21ac747867a3073e1c0ed310b7
Eunjae Lee <kckjn97@gmail.com>
2019-09-02 14:39:00 +0900

    10871

---
+++ 10871/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, x, t;
+	cin >> n >> x;
+	for(int i=0; i<n; i++){
+		cin >> t;
+		if(t<x){
+			cout << t << ' ';
+		}
+	}
+	cout << endl;
+}
