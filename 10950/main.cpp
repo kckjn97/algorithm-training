a3c5dcafad62972f143f87d6cce45b8cf7796fdd
Eunjae Lee <kckjn97@gmail.com>
2019-09-02 15:18:41 +0900

    10950

---
+++ 10950/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	int a, b;
+	for(int test_case=0; test_case<T; test_case++){
+		cin >> a >> b;
+		cout << a+b << endl;
+	}
+}
