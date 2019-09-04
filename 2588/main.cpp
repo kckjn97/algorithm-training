e2c8904d08f42197626b16c018f6965ef31713a9
Eunjae Lee <kckjn97@gmail.com>
2019-09-04 15:33:48 +0900

    2588

---
+++ 2588/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	int a;
+	char b[4];
+	cin >> a;
+	cin >> b;
+
+	cout << a*(b[2]-'0') << endl;
+	cout << a*(b[1]-'0') << endl;
+	cout << a*(b[0]-'0') << endl;
+	cout << a*atoi(b) << endl;
+}
