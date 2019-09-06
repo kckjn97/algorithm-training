79e13176df0bea033fc0f802113c2b2c395a5c41
Eunjae Lee <kckjn97@gmail.com>
2019-09-06 16:40:53 +0900

    11022

---
+++ 11022/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	for(int test_case=0; test_case<T; test_case++){
+		int a, b;
+		cin >> a >> b;
+		cout << "Case #" << test_case+1 << ": " << a << " + " << b << " = " << a+b << endl;
+	}
+}
