4dc4a4b63883f94bb43e896ba1ecdf4b144bfd14
Eunjae Lee <kckjn97@gmail.com>
2019-09-06 14:11:41 +0900

    3053

---
+++ 3053/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	double r;
+	cin >> r;
+	cout << fixed;
+	cout.precision(6);
+	cout << r*r*M_PI << endl;
+	cout << r*r*2 << endl;
+}
