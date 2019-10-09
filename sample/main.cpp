00c92a6553877d6e7e8eaa5995cc44452b305f98
Eunjae Lee <kckjn97@gmail.com>
2019-10-09 17:27:08 +0900

    sample

--- sample/main.cpp
+++ sample/main.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main() {
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	int T;
 	cin >> T;
 	for(int test_case=0; test_case<T; test_case++){
